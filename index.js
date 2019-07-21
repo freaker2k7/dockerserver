c68a101927109892372eeda1b35cf9b0244e074c
Evgeny Kolyakov <thenetfreaker@gmail.com>
2019-07-21 22:40:43 +0300

    Seperation

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 #!/usr/bin/env node
 
-var dockerCLI = require('docker-cli-js');
 var express = require('express');
 var throttle = require('express-throttle');
 var bodyParser = require('body-parser');
 var uuid = require('uuid/v4');
 var os = require('os');
 
+var actions = require('./lib/actions.js');
+
 var app = express();
-var docker = new dockerCLI.Docker();
 
 var context = process.env.DS_CONTEXT || os.homedir();
 var port = parseInt(process.env.DS_PORT) || 1717;
 var token = process.env.DS_TOKEN || 'xxxxxxxxxxxxxxxxxxxxxxxx';
 
-token = (Buffer.from && Buffer.from(token) || new Buffer(token)).toString('base64');
-
-var handle_error = function(req, res) {
-	return function(err) {
-		res.status(500).send(err);
-	};
-};
+token = 'Basic ' + (Buffer.from && Buffer.from(token) || new Buffer(token)).toString('base64');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -33,72 +27,17 @@ app.use(function (req, res, next) {
 	}
 });
 
-app.get('/', throttle({ 'burst': 5, 'period': '1s' }), function(req, res) {
-	docker.command('ps -a').then(function(data) {
-		// console.debug('GET:', data);
-		res.json(data.containerList || []);
-	}).catch(handle_error(req, res));
-});
+app.get('/', throttle({ 'burst': 5, 'period': '1s' }), actions.ps);
 
-app.get('/:id', throttle({ 'burst': 5, 'period': '1s' }), function(req, res) {
-	docker.command('logs ' + req.params.id).then(function(data) {
-		// console.debug('GET:', req.params.id, '::', data);
-		res.json(data);
-	}).catch(handle_error(req, res));
-});
+app.get('/:id', throttle({ 'burst': 5, 'period': '1s' }), actions.logs);
 
-app.put('/', throttle({ 'burst': 1, 'period': '1s' }), function(req, res) {
-	console.log(req.body);
-	if (!req.body || !req.body.image) {
-		return res.status(400).send();
-	}
-	
-	var ports = '';
-	var volumes = '';
-	var data = '';
-	var detach = '';
-	var remove = '';
-	
-	if (req.body.ports) {
-		for (var k in req.body.ports) {
-			ports += ' -p ' + k + ':' + req.body.ports[k];
-		}
-	}
-	if (req.body.volumes) {
-		for (var k in req.body.volumes) {
-			volumes += ' -v ' + k.replace(/^\.$/, context) + ':' + req.body.volumes[k];
-		}
-	}
-	if (req.body.detach) {
-		detach = ' --detach';
-	}
-	if (req.body.remove) {
-		remove = ' --rm';
-	}
-	if (req.body.data) {
-		data = ' ' + req.body.data;
-	}
-	
-	docker.command('run --name ' + (req.body.name || uuid()) + ports + volumes + remove + detach + ' ' + req.body.image + data).then(function(data) {
-		// console.debug('PUT:', data);
-		res.json(data);
-	}).catch(handle_error(req, res));
-});
+app.put('/', throttle({ 'burst': 1, 'period': '1s' }), actions.run);
 
-app.delete('/:id', throttle({ 'burst': 3, 'period': '1s' }), function(req, res) {
-	if (!req.params.id) {
-		return res.status(400).send();
-	}
-	
-	docker.command('rm -f ' + req.params.id).then(function(data) {
-		// console.debug('DELETE (RM):', data);
-		res.json(data);
-	}).catch(handle_error(req, res));
-});
+app.delete('/:id', throttle({ 'burst': 3, 'period': '1s' }), actions.rm);
 
 app.listen(port);
 
 console.log('Serving on http://localhost:' + port);
 
 
-module.exports = app;
+module.exports = Object.assign(actions, {'_app': app});
